a4c034f3aa6fd0a8ea8b01b44aa805220eca2b45
yuorei <108039575+yuorei@users.noreply.github.com>
2024-08-07 15:01:46 +0900

    tokenからのUserIDを使用

--- src/usecase/port/organization_port.go
+++ src/usecase/port/organization_port.go
@@ -7,11 +7,11 @@ import (
 )
 
 type OrganizationInputPort interface {
-	CreateOrganization(ctx context.Context, organizationID, clientID, ClientSecret string) (*domain.Organization, error)
+	CreateOrganization(ctx context.Context, organizationID, clientID, ClientSecret, userID string) (*domain.Organization, error)
 	CreateTmpOrganization(ctx context.Context, organization *domain.Organization, clientID, ClientSecret string) (*domain.Organization, error)
 }
 
 type OrganizationRepository interface {
-	DBCreateOrganization(ctx context.Context, organizationID, clientID, ClientSecret string) (*domain.Organization, error)
+	DBCreateOrganization(ctx context.Context, organizationID, clientID, ClientSecret, userID string) (*domain.Organization, error)
 	TmpSaveRedisCreateOrganization(ctx context.Context, organization *domain.Organization, clientID, ClientSecret string) (*domain.Organization, error)
 }
