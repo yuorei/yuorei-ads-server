13fe73fb0caf2a34d244ebc0f6ccb7cd5c3bb243
yuorei <108039575+yuorei@users.noreply.github.com>
2024-08-13 15:40:50 +0900

    動画広告の作成

--- src/domain/ads.go
+++ src/domain/ads.go
@@ -1,6 +1,8 @@
 package domain
 
-import "time"
+import (
+	"time"
+)
 
 type Campaign struct {
 	CampaignID string
@@ -15,6 +17,62 @@ type Campaign struct {
 	DeleteAt   *time.Time
 }
 
+type Ad struct {
+	AdID       string
+	CampaignID string
+	AdType     string
+	IsApproval bool
+	IsOpen     bool
+	AdLink     string
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+	DeleteAt   *time.Time
+	Tags       []string
+}
+
+type AdVideo struct {
+	AdID         string
+	Title        string
+	Description  string
+	VideoUrl     string
+	ThumbnailUrl string
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
+	DeleteAt     *time.Time
+}
+
+type Video struct {
+	ID                string
+	Title             string
+	Description       *string
+	ThumbnailImageUrl string
+	UserId            string
+	Tags              []string
+	Private           bool
+	Adult             bool
+	ExternalCutout    bool
+	IsAd              bool
+}
+
+type UploadVideo struct {
+	ID               string
+	VideoContentType string
+
+	ImageContentType string
+	Tags             []string
+	Title            string
+	Description      *string
+	IsPrivate        bool
+	IsAdult          bool
+	IsExternalCutout bool
+	IsAd             bool
+}
+
+type ThumbnailImage struct {
+	ID          string
+	ContentType string
+}
+
 func NewCampaign(campaignID, userID, name string, budget int, startDate, endDate time.Time, isApproval bool, createdAt time.Time, updatedAt time.Time, deletedAt *time.Time) *Campaign {
 	return &Campaign{
 		CampaignID: campaignID,
@@ -27,5 +85,33 @@ func NewCampaign(campaignID, userID, name string, budget int, startDate, endDate
 		CreatedAt:  createdAt,
 		UpdatedAt:  updatedAt,
 		DeleteAt:   deletedAt,
+	}
+}
+
+func NewAd(adiD, campaignID, adType string, isApproval, isOpen bool, adLink string, tags []string, createdAt, updatedAt time.Time, deletedAt *time.Time) *Ad {
+	return &Ad{
+		AdID:       adiD,
+		CampaignID: campaignID,
+		AdType:     adType,
+		IsApproval: isApproval,
+		IsOpen:     isOpen,
+		AdLink:     adLink,
+		CreatedAt:  createdAt,
+		UpdatedAt:  updatedAt,
+		DeleteAt:   deletedAt,
+		Tags:       tags,
+	}
+}
+
+func NewAdVideo(adID, title, description, videoUrl, thumbnailUrl string, createdAt, updatedAt time.Time, deletedAt *time.Time) *AdVideo {
+	return &AdVideo{
+		AdID:         adID,
+		Title:        title,
+		Description:  description,
+		VideoUrl:     videoUrl,
+		ThumbnailUrl: thumbnailUrl,
+		CreatedAt:    createdAt,
+		UpdatedAt:    updatedAt,
+		DeleteAt:     deletedAt,
 	}
 }
--- src/driver/router/router.go
+++ src/driver/router/router.go
@@ -13,6 +13,7 @@ import (
 	"github.com/yuorei/yuorei-ads/middleware"
 	"github.com/yuorei/yuorei-ads/src/adapter/infrastructure"
 	"github.com/yuorei/yuorei-ads/src/adapter/presentation"
+	"github.com/yuorei/yuorei-ads/src/adapter/presentation/rest"
 	"github.com/yuorei/yuorei-ads/src/usecase"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
@@ -21,6 +22,11 @@ import (
 func NewRouter() {
 	infra := infrastructure.NewInfrastructure()
 	repository := usecase.NewRepository(infra)
+
+	// rest handler
+	handler := rest.NewHandler(repository)
+
+	// grpc handler
 	ads := presentation.NewAdsServer(repository)
 	user := presentation.NewUserServer(repository)
 	organization := presentation.NewOrganizationServer(repository)
@@ -35,11 +41,20 @@ func NewRouter() {
 	mux.Handle(userv1connect.NewUserServiceHandler(user))                         // TODO: interfaceを実装していく
 	mux.Handle(organizationv1connect.NewOrganizationServiceHandler(organization)) // TODO: interfaceを実装していく
 
+	mux.HandleFunc("/ad/video", handler.UploadAdVideoHandler)
+
 	host := os.Getenv("IP") + ":" + os.Getenv("PORT")
 	fmt.Println("Server is running on " + host)
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*.yuorei.com", "http://localhost:3000"},
+		AllowedMethods:   []string{http.MethodGet, http.MethodPost, http.MethodDelete},
+		AllowCredentials: true,
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"*"},
+	})
 	log.Fatalln(http.ListenAndServe(
 		host,
-		cors.AllowAll().Handler(
+		c.Handler(
 			firebaseApp.AuthMiddleware(
 				// Use h2c so we can serve HTTP/2 without TLS.
 				// HTTP1.1リクエストはHTTP/2にアップグレードされる
