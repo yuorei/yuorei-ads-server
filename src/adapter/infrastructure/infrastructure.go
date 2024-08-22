fcb42f0f8b99c48da3fa5df3f3d616782a9592fd
yuorei <108039575+yuorei@users.noreply.github.com>
2024-08-22 14:47:26 +0900

    bigqueryクライアント追加

--- src/adapter/infrastructure/infrastructure.go
+++ src/adapter/infrastructure/infrastructure.go
@@ -1,9 +1,11 @@
 package infrastructure
 
 import (
+	"cloud.google.com/go/bigquery"
 	"github.com/redis/go-redis/v9"
 	"github.com/yuorei/yuorei-ads/src/driver/client"
 	"github.com/yuorei/yuorei-ads/src/driver/db"
+	"github.com/yuorei/yuorei-ads/src/driver/googlecloud"
 	r "github.com/yuorei/yuorei-ads/src/driver/redis"
 )
 
@@ -11,6 +13,7 @@ type Infrastructure struct {
 	db        *db.DB
 	redis     *redis.Client
 	yuovision *client.ClientYuoVision
+	bigquery  *bigquery.Client
 }
 
 func NewInfrastructure() *Infrastructure {
@@ -18,5 +21,6 @@ func NewInfrastructure() *Infrastructure {
 		db:        db.NewMySQLDB(),
 		redis:     r.ConnectRedis(),
 		yuovision: client.NewClientYuoVision(),
+		bigquery:  googlecloud.NewBigQuery(),
 	}
 }
