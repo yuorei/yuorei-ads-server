15f1df57264d184bd0e7e1b2a31b1268ff79f2f7
yuorei <108039575+yuorei@users.noreply.github.com>
2025-01-07 10:22:01 +0900

    authの修正

--- middleware/auth.go
+++ middleware/auth.go
@@ -10,11 +10,10 @@ import (
 // AuthMiddleware is a middleware for authenticating requests with Firebase
 func (f *FirebaseApp) AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ctx := context.Background()
 		authHeader := r.Header.Get("Authorization")
-		if authHeader == "" {
+		if authHeader == "Bearer" { // 空の時
 			log.Println("Authorization header is missing")
-			ctx = context.WithValue(r.Context(), "Authorization", "Authorization header is missing")
+			ctx := context.WithValue(r.Context(), "Authorization", "Authorization header is missing")
 			next.ServeHTTP(w, r.WithContext(ctx))
 			return
 		}
@@ -24,7 +23,7 @@ func (f *FirebaseApp) AuthMiddleware(next http.Handler) http.Handler {
 		if err != nil {
 			if strings.Contains(err.Error(), "ID token has expired") {
 				log.Println("ID token has expired")
-				ctx = context.WithValue(r.Context(), "Authorization", "ID token has expired")
+				ctx := context.WithValue(r.Context(), "Authorization", "ID token has expired")
 				next.ServeHTTP(w, r.WithContext(ctx))
 				return
 			}
@@ -35,7 +34,7 @@ func (f *FirebaseApp) AuthMiddleware(next http.Handler) http.Handler {
 
 		// Token is valid, set the token in the context and call the next handler
 		key := "uid"
-		ctx = context.WithValue(r.Context(), key, token.UID)
+		ctx := context.WithValue(r.Context(), key, token.UID)
 
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
